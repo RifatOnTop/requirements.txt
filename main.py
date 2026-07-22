678708fb5fce05a033f592049240da4962989811
RifatOnTop <rifat517517@gmail.com>
2026-07-23 00:25:51 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config import TOKEN
+from handlers import router
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+dp.include_router(router)
+
+async def main():
+    print("🚀 HSC Batch 2026 Bot Started...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
