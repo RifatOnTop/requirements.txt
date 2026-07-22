d525734c1fffa024382b8afdfa690f67b13348c4
RifatOnTop <rifat517517@gmail.com>
2026-07-23 00:18:49 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.getenv("8519287610:AAHNEACw3qHuVS-ycvy0_TxSrb84IdB8fGQ")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", "6369158938"))
+
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "@https://t.me/hsccbatch2026")
